83b7186b8ea22b0dab2e11f0e873cd8ee3838eed
TechnikTil <til.tjardes@gmail.com>
2025-04-02 16:46:38 -0600

    whoops

--- scripts/skipping cuz i dont wanna play allat.lua
+++ scripts/skipping cuz i dont wanna play allat.lua
@@ -1,6 +1,6 @@
 function onUpdatePost(elapsed)
 	-- uncomment the true if you want, i do NOT care in the slightest
-	if getPropertyFromClass('states.PlayState', 'chartingMode') or true then
+	if getPropertyFromClass('states.PlayState', 'chartingMode') then
 		if keyboardJustPressed('ONE') then
 			endSong();
 		end
